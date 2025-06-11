c66bc4556a5a115e2bea8b35ecb163b03c2251bf
Christopher West <cpw9783@rit.edu>
2025-06-10 21:36:55 -0400

    gatsby setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the Portfolio Site</h1>
+      <p>blah blah blah.</p>
+    </div>
+  )
+}
