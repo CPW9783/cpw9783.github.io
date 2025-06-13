12f705313042c024230a80d8244dac7b7957aef4
Christopher West <cpw9783@rit.edu>
2025-06-13 12:50:17 -0400

    added site sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import * as React from "react"
+import Header from "../components/header"
+import Skills from "../components/skills"
+import Projects from "../components/projects"
+import Contact from "../components/contact"
+import About from "../components/about"
 
 export default function LandingPage() {
   return (
     <div>
       <h1>Welcome to the Portfolio Site</h1>
       <p>blah blah blah.</p>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
+    
   )
 }
