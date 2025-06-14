28383c6827312795f52c8ad4183f02320d7b1545
Christopher West <cpw9783@rit.edu>
2025-06-14 17:52:56 -0400

    nav bar transitions

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -15,32 +15,32 @@ const Projects: React.FC = () => {
           </div>
           <div className="flex justify-center flex-row w-full  lg:items-start gap-6 lg:gap-10 xl:gap-16">
             <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 sm:gap-12 lg:gap-16 mt-6">
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={imprint} alt="Imprint Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">Imprint</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
                 </li>
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={teachme} alt="TeachMe Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">TeachMe</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
                 </li>
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={munsonrevenge} alt="Munson Revenge Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">Munson Revenge</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
                 </li>
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={comix} alt="Comix Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">Comix</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
                 </li>
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={nightclub} alt="Nightclub Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">Nightclub</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
                 </li>
-                <li className="flex flex-col items-center w-full max-w-[451px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
+                <li className="flex flex-col items-center w-full max-w-[400px] h-auto aspect-[451/427] rounded-2xl bg-[#3A5B70] p-4">
                   <img src={fuzzer} alt="Fuzzer Project" className="mt-2 max-h-[200px] object-contain" />
                   <span className="text-xl m-2 underline font-medium">Fuzzer</span>
                   <p className="my-1 mx-2 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit enim turpis, nec.</p>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,18 @@ import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
 import About from "../components/About"
+import { useSectionObserver } from "../utils/useSectionObserver";
+
 
 export default function LandingPage() {
+  const sectionIds = ["home", "about", "skills", "projects", "contact"];
+  const activeSection = useSectionObserver(sectionIds);
+
+  const scrollToSection = (id: string) => {
+    const el = document.getElementById(id);
+    if (el) el.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="flex flex-col px-4 sm:px-6 md:px-10 lg:px-20">
       <div className="flex w-full sticky pb-3 top-0 z-50 bg-[#09141F]">
@@ -13,15 +23,29 @@ export default function LandingPage() {
           <div className="flex w-full justify-center lg:justify-start lg:w-auto lg:ml-20 text-4xl">[CW]</div>
           <div className="hidden lg:flex justify-end w-full mt-3 mr-20">
             <ul className="flex space-x-5">
-              <li><a href="#home">Home</a></li>
-              <div>|</div>
-              <li><a href="#about">About</a></li>
-              <div>|</div>
-              <li><a href="#skills">Skills</a></li>
-              <div>|</div>
-              <li><a href="#projects">Projects</a></li>
-              <div>|</div>
-              <li><a href="#contact">Contact</a></li>
+              {sectionIds.map((id) => (
+                <li key={id}>
+                  <button
+                    onClick={() => scrollToSection(id)}
+                    className={`
+                      relative 
+                      transition-all 
+                      duration-300 
+                      after:content-[''] 
+                      after:absolute 
+                      after:left-0 
+                      after:bottom-0 
+                      after:h-[2px] 
+                      after:bg-[#A5BADA] 
+                      after:transition-all 
+                      after:duration-300 
+                      ${activeSection === id ? "text-[#A5BADA] after:w-full" : "after:w-0"}
+                    `}
+                  >
+                    {id.charAt(0).toUpperCase() + id.slice(1)}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
         </nav>
